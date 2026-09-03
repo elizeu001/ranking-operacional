63b7afc6f108b30058d5346d2a5a3098873034d2
elizeu001 <elizeu.soares@souuni.com>
2026-09-03 15:59:36 -0400

    Add API handler for Operational Ranking

--- api/index.js
+++ api/index.js
@@ -1,1 +1,9 @@
 
+// API do Ranking Operacional
+
+export default async function handler(req, res) {
+  res.status(200).json({
+    status: "ok",
+    mensagem: "API do Ranking Operacional funcionando!"
+  });
+}
